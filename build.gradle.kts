b7ffc83e84687e518b333e5e22bab4a38e8af207
Hanno Embregts <hanno.embregts@infosupport.com>
2025-09-14 20:35:01 +0200

    Start over

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
-    kotlin("jvm") version "1.9.23"
+    kotlin("jvm") version "2.2.20"
 }
 
-group = "com.hannotify"
+group = "com.infosupport.kotlin"
 version = "1.0-SNAPSHOT"
 
 repositories {
@@ -12,6 +12,9 @@ repositories {
 dependencies {
     testImplementation(kotlin("test"))
     testImplementation("com.willowtreeapps.assertk:assertk:0.28.1")
+    implementation("org.jetbrains.kotlin:kotlin-reflect:2.2.20")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
+
 }
 
 tasks.test {
--- src/main/kotlin/com/hannotify/ch3/useextension/UseExtensionFunction.kt
+++
@@ -1,8 +0,0 @@
-package com.hannotify.ch3.useextension
-
-import com.hannotify.ch3.lastChar
-
-fun main() {
-    val s = "Kotlin"
-    println(s.lastChar())
-}
