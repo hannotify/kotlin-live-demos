d0d14ccc3ed165666fb0f283268707025f5d44d4
Hanno Embregts <hanno.embregts@infosupport.com>
2026-05-29 14:28:47 +0200

    Demos for Chapters 4 & 5

---
+++ src/main/kotlin/com/infosupport/kotlin/ch5/lambdas/Musician.kt
@@ -0,0 +1,27 @@
+package com.infosupport.demos.ch5.lambdas
+
+import com.infosupport.demos.ch5.lambdas.Instrument.Companion.guitar
+import com.infosupport.demos.ch5.lambdas.Instrument.Companion.keyboard
+import com.infosupport.demos.ch5.lambdas.Instrument.Companion.vocal
+
+data class Person(val name: String = "", var age: Int = 0, var instruments: List<Instrument> = emptyList())
+
+data class Instrument(val name: String) {
+    constructor(i: Int) : this(i.toString())
+
+    companion object {
+        val keyboard = Instrument("Keyboard")
+        val guitar = Instrument("Guitar")
+        val vocal = Instrument("Vocal")
+    }
+}
+
+val people = listOf(
+    Person("Alice", 29),
+    Person("Bob", 31),
+    Person("Kurt", 27, listOf(guitar, vocal)),
+    Person("Jim", 27, listOf(keyboard, vocal)),
+    Person("Amy", 27, listOf(vocal)),
+    Person("Janis", 27, listOf(vocal))
+)
+
